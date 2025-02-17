a4ddb354cdfabec98ba6e1201b26b50e63bad34c
fellipense <kingzinhu@hotmail.com>
2025-02-17 00:51:15 -0300

    ++

--- mint.py
+++ mint.py
@@ -1,9 +1,7 @@
 import random
 import json
 
-parts = []
-types = []
-used = []
+part_types = []
 
 # Loads the JSON file
 with open('parts.json', 'r') as file:
@@ -11,7 +9,21 @@ with open('parts.json', 'r') as file:
 
 # Iterate through the list of objects
 for obj in data:
-    parts.append(obj)
-    part_name = obj['part']
-    if types.count(part_name) == 0:
-        types.append(part_name)
+    if obj['part'] not in part_types:
+        part_types.append(obj['part'])
+
+def draw():
+    for t in part_types:
+
+        temp_items = []
+
+        for d in data:
+            if d['part'] == t:
+                temp_items.append(d)
+
+        temp_items.sort(key=lambda item: item['chance'])
+
+        dice = random.uniform(0, 1)
+
+        for item in temp_items:
+            if dice <= item['chance']: return item
